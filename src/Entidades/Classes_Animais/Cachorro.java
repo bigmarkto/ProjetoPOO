5c2c2c7dc7a6f31ec84f6dc09d916dbd09d3d9ef
Emanuel Gomes <eg121745@gmail.com>
2025-05-16 20:44:35 -0300

    Adicionar classes Cachorro e Gato com atributos e construtores

---
+++ src/Entidades/Classes_Animais/Cachorro.java
@@ -0,0 +1,15 @@
+package Entidades.Classes_Animais;
+
+public class Cachorro {
+
+    public String nome;
+    public int idade;
+    public String Dono;
+
+    public Cachorro(String nome, int idade, String dono) {
+        this.nome = nome;
+        this.idade = idade;
+        this.Dono = dono;
+    }
+    
+}
---
+++ src/Entidades/Classes_Animais/Gato.java
@@ -0,0 +1,14 @@
+package Entidades.Classes_Animais;
+
+public class Gato {
+    public String nome;
+    public int idade;
+    public String Dono;
+
+    public Gato(String nome, int idade, String dono) {
+        this.nome = nome;
+        this.idade = idade;
+        this.Dono = dono;
+    }
+    
+}
