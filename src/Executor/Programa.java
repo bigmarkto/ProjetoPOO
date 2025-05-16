b0cb386c84bb6e2322939b8d66b40423be6fff43
Neemias Vasconcelos <167483565+neemo-o@users.noreply.github.com>
2025-05-16 20:06:12 -0300

    v0.0.1

--- src/Executor/Programa.java
+++ src/Executor/Programa.java
@@ -1,7 +1,42 @@
 package Executor;
 
+import java.util.Scanner;
+
 public class Programa {
-    public static void main(String [] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        // Váriaveis iniciais
+        int op;
+
+        // Menu de opções
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---== MENU DE OPÇÕES ==---");
+        System.out.println("1 - Cadastrar animal no sistema");
+        System.out.println("2 - Alterar informações do animal");
+        System.out.println("3 - Remover animal do sistema");
+        System.out.println("0 - Sair");
+        System.out.print("Escolha uma opção: ");
+        op = sc.nextInt();
+
+        do {
+            switch (op) {
+                case 1:
+                    System.out.println("Você escolheu a opção 1");
+                    break;
+                case 2:
+                    System.out.println("Você escolheu a opção 2");
+                    break;
+                case 3:
+                    System.out.println("Você escolheu a opção 3");
+                    break;
+                case 0:
+                    System.out.println("Obrigado por usar o sistema! Até a proxima!");
+                    break;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+                    break;
+            }
+        } while (op != 0);
+
+        sc.close();
     }
 }
